131eec763de25f784161b113d30efc6b71979ad6
shashwatkhadka <khadkashashwat@gmail.com>
2022-11-09 16:21:58 +0545

    Created Product Schema, Controller, Route
    create,getall,update,delete,get product details controllers created, routes created, schema built, PRIMARY PRODUCT MODULE COMPLETED

--- backend/controllers/c_product.js
+++ backend/controllers/c_product.js
@@ -1,5 +1,12 @@
 const Product=require("../models/m_product")
 
+//get all product
+exports.getAllproducts=async(req,res)=>{
+    const allproducts=await Product.find();
+
+    res.status(200).json({success:true,allproducts})
+}
+
 //create product
 exports.createproduct=async(req,res,next)=>{
     const product=await Product.create(req.body);
@@ -7,9 +14,45 @@ exports.createproduct=async(req,res,next)=>{
     res.status(201).json({success:true, product})
 }
 
-//get product
-exports.getAllproducts=async(req,res)=>{
-    const allproducts=await Product.find();
+//update product
+exports.updateproduct=async(req,res)=>{
+    let product=await Product.findById(req.params.id);
+    //storing the info about the product in "product" by searching
+    //in the database by the product's "id" 
 
-    res.status(200).json({success:true,allproducts})
+    if(!product){
+        return res.status(500).json({success:false,message:"Product not found"})
+    }
+
+    product= await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindandModify:false
+    })
+
+    res.status(200).json({success:true, product})
+}
+
+//delete product
+exports.deleteproduct=async(req,res,next)=>{
+    const product=await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({success:false,message:"Product not found"})
+    }
+
+    await Product.remove();
+
+    res.status(200).json({success:true, message:"Product Deleted Successfully"})
+}
+
+//get product detail
+exports.getproductdetail=async(req,res)=>{
+    const product=await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({success:false,message:"Product not found"})
+    }
+
+    res.status(200).json({success:true, product})
 }
